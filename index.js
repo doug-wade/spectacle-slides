85dd6b55c3ca3ce41a049705cf0cd97660a14ae2
Doug Wade <douglas.b.wade@gmail.com>
2022-04-18 11:29:52 -0700

    Add js debugging mini-talk

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Presentation from "./presentation/unit-testing-in-vue";
+import Presentation from "./presentation/debugging-javascript";
 
 ReactDOM.render(<Presentation />, document.getElementById("root"));
