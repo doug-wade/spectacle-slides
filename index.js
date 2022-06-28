69bf76225e73efd6a7037720704f669a0d1a7670
Doug Wade <douglas.b.wade@gmail.com>
2022-06-28 11:34:53 -0700

    add glue/campsite work

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Presentation from "./presentation/data-table-strawman";
+import Presentation from "./presentation/glue-and-campsite-work";
 
 ReactDOM.render(<Presentation />, document.getElementById("root"));
