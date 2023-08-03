392960c897eb2dd86357eb421679e6a3e86c19fe
Doug Wade <doug.wade@skilljar.com>
2023-08-03 14:56:16 -0700

    feat: Add Tybalt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Presentation from "./presentation/course-platform-vnext";
+import Presentation from "./presentation/tybalt";
 
 ReactDOM.render(<Presentation />, document.getElementById("root"));
