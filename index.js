a57830c609db8a03cdf44ee3509411e1313b4de2
Douglas Wade <douglas.b.wade@gmail.com>
2024-01-31 10:33:02 -0800

    remove deploy script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Presentation from "./presentation/dual-publish-node-deno";
+import Presentation from "./presentation/just-dont-say-just";
 
 ReactDOM.render(<Presentation />, document.getElementById("root"));
